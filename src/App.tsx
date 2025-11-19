b9b685eb05b13cad9bc163406d7d1c8871979021
Condro Wiyono <condro@outlook.co.id>
2025-11-19 13:06:03 +0700

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ImageTextMatch from './components/ImageTextMatch'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <ImageTextMatch />
+    </div>
+  )
+}
+
+export default App
